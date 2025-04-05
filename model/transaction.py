f2f3562e243fafff79b7823797325644e6d20804
Durgesh <durgesh.bhuvanagiri@bottomline.com>
2025-04-05 13:33:44 +0530

    Enhanced TransactionSchema and added CORS support

--- model/transaction.py
+++ model/transaction.py
@@ -3,12 +3,22 @@ from datetime import datetime  # Fixed import
 from marshmallow import fields, Schema, post_load
 
 class Transaction(object):
-    def __init__(self, id, amount, user, transactionType, created=None):
+    def __init__(self, id, transactionAmount, customerId, merchantId, 
+            transactionType, transactionMode, currency, ipAddress, locationId, 
+            terminalId, vpnUsed, mcc, created=None, status=None, message=None):
         self.id = id
-        self.amount = amount
-        self.user = user
+        self.transactionAmount = transactionAmount
+        self.customerId = customerId
+        self.merchantId = merchantId
+        self.transactionMode = transactionMode
         self.transactionType = transactionType
         self.created = created or datetime.now()  # Ensure created is a datetime object
+        self.currency = currency
+        self.ipAddress = ipAddress
+        self.locationId = locationId
+        self.terminalId = terminalId
+        self.vpnUsed = vpnUsed
+        self.mcc = mcc
 
     def __repr__(self):
         return '<Transaction(name={self.id!r})>'.format(self=self)
@@ -19,10 +29,21 @@ class Transaction(object):
 
 class TransactionSchema(Schema):
     id = fields.Integer()
-    amount = fields.Float()  # Changed from Decimal to Float
+    transactionAmount = fields.Float()  # Changed from Decimal to Float
     transactionType = fields.Str()
     user = fields.Str()
     created = fields.DateTime()  # Ensure proper serialization/deserialization
+    status = fields.Str()
+    message = fields.Str()
+    customerId = fields.Integer()
+    merchantId = fields.Integer()
+    transactionMode = fields.Str()
+    currency = fields.Str()
+    ipAddress = fields.Str()
+    locationId = fields.Integer()
+    terminalId = fields.Integer()
+    mcc = fields.Str()
+    vpnUsed = fields.Boolean()
 
     @post_load
     def make_transaction(self, data, **kwargs):
